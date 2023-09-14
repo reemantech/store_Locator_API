82e94b0100b7373c74598b32dcd46c49269d68d0
Shivam Kumar <98273838+reemantech@users.noreply.github.com>
2023-09-15 04:34:38 +0530

    points are visible

--- public/js/map.js
+++ public/js/map.js
@@ -1,3 +1,16 @@
+const map = new ol.Map({
+  target: "map",
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM(),
+    }),
+  ],
+  view: new ol.View({
+    center: [0, 0],
+    zoom: 2,
+  }),
+});
+
 async function getStores() {
   const res = await fetch("/api/v1/stores");
   const dataJson = await res.json();
@@ -25,16 +38,18 @@ async function getStores() {
   for (let i = 0; i < stores.length; i++) {
     let store = stores[i];
     // console.log(store);
-    // console.log(store.geometry.coordinates[0],store.geometry.coordinates[1]);
+   
+    const point = ol.proj.fromLonLat([
+      store.geometry.coordinates[0],
+      store.geometry.coordinates[1],
+    ]);
+    // console.log(point);
 
     features.push(
       new ol.Feature({
-        geometry: new ol.geom.Point(
-          ol.proj.fromLonLat([
-            store.geometry.coordinates[0],
-            store.geometry.coordinates[1],
-          ])
-        ),
+        geometry: new ol.geom.Point(point),
+        name: `${store.storeId}`,
+        labelPoint: point,
       })
     );
   }
@@ -57,19 +72,7 @@ async function getStores() {
   });
   // console.log(vectorLayer, vectorLayer.length);
 
-  const map = new ol.Map({
-    target: "map",
-    layers: [
-      new ol.layer.Tile({
-        source: new ol.source.OSM(),
-      }),
-      vectorLayer,
-    ],
-    view: new ol.View({
-      center: [0, 0],
-      zoom: 2,
-    }),
-  });
+  map.addLayer(vectorLayer);
 }
 
 getStores();
